a4957328d0706ab431da3082daa51899b12c6023
wilderbuchanan <wilderbuchanan@Wilders-MacBook-Pro-2.local>
2023-02-28 00:44:08 -0800

    New Jobs!

--- jobScrape.py
+++ jobScrape.py
@@ -32,7 +32,7 @@ allJobs = "https://www.linkedin.com/jobs/search/?currentJobId=3459209529&f_E=1&f
 state_codes = {'AL': 'Alabama', 'AK': 'Alaska', 'AZ': 'Arizona', 'AR': 'Arkansas', 'CA': 'California', 'CO': 'Colorado', 'CT': 'Connecticut', 'DE': 'Delaware', 'FL': 'Florida', 'GA': 'Georgia', 'HI': 'Hawaii', 'ID': 'Idaho', 'IL': 'Illinois', 'IN': 'Indiana', 'IA': 'Iowa', 'KS': 'Kansas', 'KY': 'Kentucky', 'LA': 'Louisiana', 'ME': 'Maine', 'MD': 'Maryland', 'MA': 'Massachusetts', 'MI': 'Michigan', 'MN': 'Minnesota', 'MS': 'Mississippi', 'MO': 'Missouri', 'MT': 'Montana', 'NE': 'Nebraska', 'NV': 'Nevada', 'NH': 'New Hampshire', 'NJ': 'New Jersey', 'NM': 'New Mexico', 'NY': 'New York', 'NC': 'North Carolina', 'ND': 'North Dakota', 'OH': 'Ohio', 'OK': 'Oklahoma', 'OR': 'Oregon', 'PA': 'Pennsylvania', 'RI': 'Rhode Island', 'SC': 'South Carolina', 'SD': 'South Dakota', 'TN': 'Tennessee', 'TX': 'Texas', 'UT': 'Utah', 'VT': 'Vermont', 'VA': 'Virginia', 'WA': 'Washington', 'WV': 'West Virginia', 'WI': 'Wisconsin', 'WY': 'Wyoming'}
 state_codes_formatted = {code: name.lower().replace(' ', '-') for code, name in state_codes.items()}
 
-keyTags = ["co-op","internship","manufactuing","autodesk inventor","finite element analysis (fea)","catia","computer-aided design (cad)","composites","aerodynamics"]
+keyTags = ["co-op","internship","manufactuing","autodesk inventor","finite element analysis (fea)","catia","computer-aided design (cad)","composites","aerodynamics", "Geometric Dimensioning & Tolerancing"," Tooling Design ","ITAR"]
 
 linkedInLogin = ("https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin")
 # One-line dictionary comprehension to format the dictionary
@@ -92,7 +92,7 @@ def harvest(link,page,priority): #0 = top of page
             pass
         tags = []
         title = e.find_element(By.CLASS_NAME,"job-card-list__title").click()
-        if len(driver.find_elements(By.XPATH, "//span[@class='artdeco-button__text' and text()='Easy Apply']")) == 0:
+        if len(driver.find_elements(By.XPATH, "//span[text()='Easy Apply']")) == 0:
             time.sleep(randomWaitTime())
             jobID = str(uuid.uuid4())
             title = e.find_element(By.CLASS_NAME,"job-card-list__title")
