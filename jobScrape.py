00c35434f4c9bddb777da225236b70a0c34febce
wilderbuchanan <wilderbuchanan@wilders-mbp-2.lan>
2023-05-23 09:24:22 -0700

    New Jobs!

--- jobScrape.py
+++ jobScrape.py
@@ -15,8 +15,9 @@ import sys
 import uuid
 import json
 import githubUpdates
+from selenium.common.exceptions import NoSuchElementException
 
-
+current_page = 1
 jobSearch = "Mechanical Engineering Internship"
 searchDestination = "United States"
 Email = "Kate19Anderson2001@gmail.com"
@@ -24,7 +25,7 @@ Password = "Ryobi&Dewault"
 
 topCompaniesInternship = "https://www.linkedin.com/jobs/search/?currentJobId=3466373427&f_C=1586%2C1384%2C18264661%2C11046103%2C83277441%2C1904%2C5383634%2C3200474%2C18293159%2C3308308%2C3591568%2C10799650%2C7602863%2C51607174%2C5770%2C18280200%2C30846%2C18067251%2C1412%2C3959849%2C10464881%2C1684122%2C6407329%2C2734%2C1882%2C42881474%2C1441%2C1483%2C18203768%2C18157704%2C7952659%2C27238745%2C20708%2C7939313%2C18238200%2C12587763%2C1472%2C10981144%2C7584134%2C34228736%2C40018%2C10891165%2C18801275%2C2706887%2C12955216%2C1999%2C737010%2C2002%2C2003%2C1116%2C2004&f_E=1&f_TPR=r2592000&geoId=103644278&keywords=Mechanical%20Engineering&location=United%20States&refresh=true&sortBy=R"
 
-topCompanies = "https://www.linkedin.com/jobs/search/?currentJobId=3422067175&f_C=1586%2C1384%2C18264661%2C11046103%2C83277441%2C1904%2C5383634%2C3200474%2C18293159%2C3308308%2C3591568%2C10799650%2C7602863%2C51607174%2C5770%2C18280200%2C18067251%2C3959849%2C1412%2C10464881%2C1684122%2C6407329%2C1882%2C2734%2C1441%2C42881474%2C18203768%2C18157704%2C7952659%2C27238745%2C20708%2C7939313%2C18238200%2C12587763%2C1472%2C10981144%2C7584134%2C34228736%2C40018%2C10891165%2C18801275%2C2706887%2C12955216%2C1999%2C737010%2C2002%2C2003%2C1116%2C2004&f_E=2%2C3&f_TPR=r2592000&geoId=103644278&keywords=Mechanical%20Engineering&location=United%20States&refresh=true&sortBy=R&start=25"
+topCompanies = "https://www.linkedin.com/jobs/search/?currentJobId=3560522221&f_C=1586%2C1384%2C18264661%2C11046103%2C83277441%2C1904%2C5383634%2C3200474%2C18293159%2C3308308%2C3591568%2C10799650%2C7602863%2C51607174%2C5770%2C18280200%2C18067251%2C3959849%2C1412%2C10464881%2C1684122%2C6407329%2C1882%2C2734%2C1441%2C42881474%2C18203768%2C18157704%2C7952659%2C27238745%2C20708%2C7939313%2C18238200%2C12587763%2C1472%2C10981144%2C7584134%2C34228736%2C40018%2C10891165%2C18801275%2C2706887%2C12955216%2C1999%2C737010%2C2002%2C2003%2C1116%2C2004&f_E=2%2C3&f_TPR=r2592000&geoId=103644278&keywords=Mechanical%20Engineering&location=United%20States&refresh=true&sortBy=R"
 
 allInternships = "https://www.linkedin.com/jobs/search/?currentJobId=3459209529&f_E=1&f_TPR=r604800&geoId=103644278&keywords=Mechanical%20Engineering%20Internship&location=United%20States&refresh=true"
 
@@ -66,134 +67,141 @@ time.sleep(randomWaitTime()*3)
 
 
 def harvest(link,type,page,priority): #0 = top of page
-    driver.get(link)
-    time.sleep(randomWaitTime()*2)
-    driver.execute_script("document.body.style.zoom='30%'")
-    time.sleep(randomWaitTime())
-#    element = driver.find_element(By.XPATH, "//button[contains(@aria-label,'Page')]")
-    element = driver.find_element(By.XPATH, "//footer[contains(@aria-label,'LinkedIn Footer Content')]")
-    driver.execute_script("arguments[0].scrollIntoView();", element)
-    time.sleep(2)
-    jobPostings = driver.find_elements(By.CLASS_NAME,"job-card-container")
-    print("found postings")
-    job_postings_list = []
-    if page == 1:
-        with open("jobs.json") as infile:
-            job_postings_list = json.load(infile)
-        job_postings_list = [job for job in job_postings_list if job['priority'] != priority]
-        with open("jobs.json", "w") as outfile:
-            json.dump(job_postings_list, outfile)
-
-    for e in jobPostings:
-
+    while True:
         try:
-            with open("jobs.json") as infile:
-                job_postings_list = json.load(infile)
-        except (FileNotFoundError):
-            pass
-        tags = []
-        title = e.find_element(By.CLASS_NAME,"job-card-list__title").click()
-        time.sleep(randomWaitTime()*2)
-        if len(driver.find_elements(By.XPATH, "//span[text()='Easy Apply']")) == 0:
+            current_page_element = driver.find_element(By.XPATH, "//li[@aria-current='true']//span")
+            current_page = int(current_page_element.text)
+            print("Processing page:", current_page)
+            driver.get(link)
+            time.sleep(randomWaitTime()*2)
+            driver.execute_script("document.body.style.zoom='30%'")
             time.sleep(randomWaitTime())
-            jobID = str(uuid.uuid4())
+        #    element = driver.find_element(By.XPATH, "//button[contains(@aria-label,'Page')]")
+            element = driver.find_element(By.XPATH, "//footer[contains(@aria-label,'LinkedIn Footer Content')]")
+            driver.execute_script("arguments[0].scrollIntoView();", element)
+            time.sleep(2)
+            jobPostings = driver.find_elements(By.CLASS_NAME,"job-card-container")
+            print("found postings")
+            job_postings_list = []
+            if page == 1:
+                with open("jobs.json") as infile:
+                    job_postings_list = json.load(infile)
+                job_postings_list = [job for job in job_postings_list if job['priority'] != priority]
+                with open("jobs.json", "w") as outfile:
+                    json.dump(job_postings_list, outfile)
 
-            try:
-                title = e.find_element(By.CLASS_NAME,"job-card-list__title")
-                title = title.text
-            except Exception:
-                title = "None"
+            for e in jobPostings:
 
-            try:
-                companyName = e.find_element(By.CLASS_NAME, "job-card-container__primary-description") #  OLD: job-card-container__company-name
-                companyName = companyName.text
-            except Exception:
-                companyName = "None"
-            try:
-                location = e.find_element(By.CLASS_NAME,"job-card-container__metadata-item")
-                location = location.text
-                state = location[-2:]
-                state_name = state_codes_formatted.get(state)
-                tags.append(state_name)
-                for t in keyTags:
-                    if t in title.lower():
-                        tags.append(t)
-                if "manufacturing" in title.lower() and "manufacturing" not in tags:
-                    tags.append("manufacturing")
-                if type == "intern":
-                    tags.append("internship")
-                if type == ("ft"):
-                    tags.append("full-time")
-            except Exception:
-                print("no location")
-            try:
-                image = e.find_element(By.XPATH, ".//img[starts-with(@id,'ember')]")
-                image = image.get_attribute("src")
-            except Exception:
-                print("no image")
-            try:
-                posted = driver.find_element(By.CLASS_NAME,"jobs-unified-top-card__posted-date")
-                posted = posted.text
-            except:
-                print("no post date")
-            buttons = driver.find_elements(By.CLASS_NAME,"jobs-apply-button")
-            print(buttons)
-            if len(buttons) > 0:
-                print("got url")
-                apply = driver.find_element(By.CLASS_NAME,"jobs-apply-button").click()
-                time.sleep(randomWaitTime())
-                driver.switch_to.window(driver.window_handles[1])
-                time.sleep(randomWaitTime())
-                applicationURL = driver.current_url
-                applicationURL = applicationURL.replace('?source=LinkedIn', '')
-                applicationURL += '?utm_source=job-board&utm_medium=website&utm_campaign=job-listing'
-                driver.close()
-                time.sleep(randomWaitTime())
-                driver.switch_to.window(driver.window_handles[0])
-                time.sleep(randomWaitTime())
-            else:
-                applicationURL = "https://www.hardwareishard.net/job-board"
-# SKILL CAPTURE HAS BEEN COMMENTED OUT FOR SPEED AND REABILITY
-    #        if len(driver.find_elements(By.XPATH, "//button[contains(@aria-label,'strong')]"))>0:
-    #            skills = driver.find_element(By.XPATH, "//button[contains(@aria-label,'strong')]").click()
-    #            time.sleep(randomWaitTime())
-    #            #caps = driver.find_elements(By.CLASS_NAME, "job-details-skill-match-status-list__unmatched-skill text-body-small")
-    #            caps = driver.find_elements(By.XPATH, "//div[contains(@aria-label,'Your')]")
-    #            for s in caps:
-    #                cappy = s.text
-    #                if cappy.lower() in keyTags:
-    #                    tags.append(cappy.lower())
-    #            time.sleep(randomWaitTime())
-    #            close = driver.find_element(By.XPATH, "//span[text()='Done']")
-    #            parent = close.find_element(By.XPATH, "..")
-    #            parent.click()
-    #        time.sleep(randomWaitTime())
+                try:
+                    with open("jobs.json") as infile:
+                        job_postings_list = json.load(infile)
+                except (FileNotFoundError):
+                    pass
+                tags = []
+                title = e.find_element(By.CLASS_NAME,"job-card-list__title").click()
+                time.sleep(randomWaitTime()*2)
+                if len(driver.find_elements(By.XPATH, "//span[text()='Easy Apply']")) == 0:
+                    time.sleep(randomWaitTime())
+                    jobID = str(uuid.uuid4())
 
-            job_posting ={
-                    "title": title,
-                    "company": companyName,
-                    "location": location,
-                    "applicationURL": applicationURL,
-                    "postedDate": posted,
-                    "imageURL": image,
-                    "tags": tags,
-                    "priority": priority
-                }
-            job_postings_list.append(job_posting)
-            with open("jobs.json", "w") as outfile:
-                json.dump(job_postings_list,outfile)
-            time.sleep(randomWaitTime())
-            #print(job_postings_list)
-            time.sleep(randomWaitTime())
-            with open("jobs.json", "r") as outfile:
-                sorted_job_list = sorted(job_postings_list, key=lambda x: int(x["priority"]))
-            with open("jobs.json", "w") as outfile:
-                json.dump(sorted_job_list, outfile)
-    githubUpdates.git_push()
-    if page != 0:
-        element = driver.find_element(By.XPATH, "//button[contains(@aria-label,'Page " + str(page) + "')]").click()
-    time.sleep(randomWaitTime()*2)
+                    try:
+                        title = e.find_element(By.CLASS_NAME,"job-card-list__title")
+                        title = title.text
+                    except Exception:
+                        title = "None"
 
+                    try:
+                        companyName = e.find_element(By.CLASS_NAME, "job-card-container__primary-description") #  OLD: job-card-container__company-name
+                        companyName = companyName.text
+                    except Exception:
+                        companyName = "None"
+                    try:
+                        location = e.find_element(By.CLASS_NAME,"job-card-container__metadata-item")
+                        location = location.text
+                        state = location[-2:]
+                        state_name = state_codes_formatted.get(state)
+                        tags.append(state_name)
+                        for t in keyTags:
+                            if t in title.lower():
+                                tags.append(t)
+                        if "manufacturing" in title.lower() and "manufacturing" not in tags:
+                            tags.append("manufacturing")
+                        if type == "intern":
+                            tags.append("internship")
+                        if type == ("ft"):
+                            tags.append("full-time")
+                    except Exception:
+                        print("no location")
+                    try:
+                        image = e.find_element(By.XPATH, ".//img[starts-with(@id,'ember')]")
+                        image = image.get_attribute("src")
+                    except Exception:
+                        print("no image")
+                    try:
+                        posted = driver.find_element(By.CLASS_NAME,"jobs-unified-top-card__posted-date")
+                        posted = posted.text
+                    except:
+                        print("no post date")
+                    buttons = driver.find_elements(By.CLASS_NAME,"jobs-apply-button")
+                    print(buttons)
+                    if len(buttons) > 0:
+                        print("got url")
+                        apply = driver.find_element(By.CLASS_NAME,"jobs-apply-button").click()
+                        time.sleep(randomWaitTime())
+                        driver.switch_to.window(driver.window_handles[1])
+                        time.sleep(randomWaitTime())
+                        applicationURL = driver.current_url
+                        applicationURL = applicationURL.replace('?source=LinkedIn', '')
+                        applicationURL += '?utm_source=job-board&utm_medium=website&utm_campaign=job-listing'
+                        driver.close()
+                        time.sleep(randomWaitTime())
+                        driver.switch_to.window(driver.window_handles[0])
+                        time.sleep(randomWaitTime())
+                    else:
+                        applicationURL = "https://www.hardwareishard.net/job-board"
+        # SKILL CAPTURE HAS BEEN COMMENTED OUT FOR SPEED AND REABILITY
+            #        if len(driver.find_elements(By.XPATH, "//button[contains(@aria-label,'strong')]"))>0:
+            #            skills = driver.find_element(By.XPATH, "//button[contains(@aria-label,'strong')]").click()
+            #            time.sleep(randomWaitTime())
+            #            #caps = driver.find_elements(By.CLASS_NAME, "job-details-skill-match-status-list__unmatched-skill text-body-small")
+            #            caps = driver.find_elements(By.XPATH, "//div[contains(@aria-label,'Your')]")
+            #            for s in caps:
+            #                cappy = s.text
+            #                if cappy.lower() in keyTags:
+            #                    tags.append(cappy.lower())
+            #            time.sleep(randomWaitTime())
+            #            close = driver.find_element(By.XPATH, "//span[text()='Done']")
+            #            parent = close.find_element(By.XPATH, "..")
+            #            parent.click()
+            #        time.sleep(randomWaitTime())
+
+                    job_posting ={
+                            "title": title,
+                            "company": companyName,
+                            "location": location,
+                            "applicationURL": applicationURL,
+                            "postedDate": posted,
+                            "imageURL": image,
+                            "tags": tags,
+                            "priority": priority
+                        }
+                    job_postings_list.append(job_posting)
+                    with open("jobs.json", "w") as outfile:
+                        json.dump(job_postings_list,outfile)
+                    time.sleep(randomWaitTime())
+                    #print(job_postings_list)
+                    time.sleep(randomWaitTime())
+                    with open("jobs.json", "r") as outfile:
+                        sorted_job_list = sorted(job_postings_list, key=lambda x: int(x["priority"]))
+                    with open("jobs.json", "w") as outfile:
+                        json.dump(sorted_job_list, outfile)
+            githubUpdates.git_push()
+            if page != 0:
+                element = driver.find_element(By.XPATH, "//button[contains(@aria-label,'Page " + str(page) + "')]").click()
+            time.sleep(randomWaitTime()*2)
+        except NoSuchElementException:
+            print("Page selector bar not found. Exiting the loop.")
+            break
 def shuffle():
     # Load the JSON data
     with open('jobs.json', 'r') as f:
