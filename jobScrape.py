371a292c347c4ec64c60e7984b03aeb6267dca1d
ubuntu <ubuntu@ip-172-31-23-66>
2023-05-24 04:19:47 +0000

    New Jobs!

--- jobScrape.py
+++ jobScrape.py
@@ -59,7 +59,7 @@ service = Service(geckodriver_path)
 
 # Configure Firefox options
 options = Options()
-options.headless = True
+options.add_argument('-headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 
