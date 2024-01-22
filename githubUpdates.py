d2e86cbeb0d7082cfd06993eec56f16c56e09dbb
wilderbuchanan <wilderbuchanan@Wilders-MacBook-Pro-2.local>
2024-01-22 00:29:41 -0800

    New Emails!

--- githubUpdates.py
+++ githubUpdates.py
@@ -1,7 +1,17 @@
-from git import Repo
+from git import Repo, exc
 
 PATH_OF_GIT_REPO = r'.git'  # make sure .git folder is properly configured
-COMMIT_MESSAGE = 'New Jobs!'
+COMMIT_MESSAGE = 'New Emails!'
+
+def git_pull():
+    try:
+        repo = Repo(PATH_OF_GIT_REPO)
+        origin = repo.remote(name='origin')
+        origin.fetch()
+        repo.git.merge('FETCH_HEAD')
+        print('Successfully pulled from the repository')
+    except exc.GitCommandError as e:
+        print(f'Error occurred while pulling: {e}')
 
 def git_push():
     try:
@@ -10,7 +20,6 @@ def git_push():
         repo.index.commit(COMMIT_MESSAGE)
         origin = repo.remote(name='origin')
         origin.push()
-    except:
-        print('Some error occured while pushing the code')
-
-git_push()
+        print('Successfully pushed to the repository')
+    except exc.GitCommandError as e:
+        print(f'Error occurred while pushing: {e}')
